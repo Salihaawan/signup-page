87194f8262c2152ebe905615f8307a6ae6948634
SALIHA RIAZ <151842064+Salihaawan@users.noreply.github.com>
2026-03-04 10:22:04 +0500

    Update Login.js

--- frontend/src/Login.js
+++ frontend/src/Login.js
@@ -8,7 +8,7 @@ export default function Login() {
 
   const handleSubmit = async (e) => {
     e.preventDefault();
-    const res = await fetch("http://52.11.164.169:5000/login", {
+    const res = await fetch(`${process.env.REACT_APP_API_URL}/login` {
       method: "POST",
       headers: { "Content-Type": "application/json" },
       body: JSON.stringify({ emailOrUsername, password }),
