c03d6588e708c93c4a011b5797f0c1c7ffcb2165
SALIHA RIAZ <151842064+Salihaawan@users.noreply.github.com>
2026-04-09 16:18:01 +0500

    Update tracing.js

--- backend/tracing.js
+++ backend/tracing.js
@@ -1,6 +1,12 @@
+const { NodeSDK } = require('@opentelemetry/sdk-node');
+const { getNodeAutoInstrumentations } = require('@opentelemetry/auto-instrumentations-node');
+const { OTLPTraceExporter } = require('@opentelemetry/exporter-trace-otlp-http');
+
 const sdk = new NodeSDK({
   traceExporter: new OTLPTraceExporter({
-    url: 'http://172.31.74.150:4319/v1/traces',
+    url: 'http://172.31.74.150:4318/v1/traces',
   }),
   instrumentations: [getNodeAutoInstrumentations()],
 });
+
+sdk.start();
