2527e9730ebaa0130f89c38c38bb76296fbb1d8f
SALIHA RIAZ <151842064+Salihaawan@users.noreply.github.com>
2026-04-09 16:04:55 +0500

    Update tracing.js

--- backend/tracing.js
+++ backend/tracing.js
@@ -1,12 +1,6 @@
-const { NodeSDK } = require('@opentelemetry/sdk-node');
-const { getNodeAutoInstrumentations } = require('@opentelemetry/auto-instrumentations-node');
-const { OTLPTraceExporter } = require('@opentelemetry/exporter-trace-otlp-http');
-
 const sdk = new NodeSDK({
   traceExporter: new OTLPTraceExporter({
-    url: 'https://unheard-michaele-chally.ngrok-free.dev/v1/traces',
+    url: 'http://172.31.74.150:4319/v1/traces',
   }),
   instrumentations: [getNodeAutoInstrumentations()],
 });
-
-sdk.start();
