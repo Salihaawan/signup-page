7075ed88e82e89327a4929e18478b190b0e75148
SALIHA RIAZ <151842064+Salihaawan@users.noreply.github.com>
2026-04-14 10:00:05 +0500

    Update tracing.js

--- backend/tracing.js
+++ backend/tracing.js
@@ -27,9 +27,6 @@ const traceExporter = new OTLPTraceExporter({
 const sdk = new NodeSDK({
   traceExporter,
   instrumentations: [getNodeAutoInstrumentations()],
-    resource: new (require('@opentelemetry/resources').Resource)({
-    'service.name': 'signup-backend',
-  }),
 });
 
 sdk.start();
